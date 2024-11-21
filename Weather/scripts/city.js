79bc58c3257b6ee350dac1df1343b3501bce9137
Jalen Battle <jalenbattle78@gmail.com>
2024-11-21 09:23:54 -0500

    working with weather

---
+++ Weather/scripts/city.js
@@ -0,0 +1,35 @@
+let cities = [
+    {
+        name: "Benbrook, TX",
+        latitude: 32.6732,
+        longitude: -97.4606
+    },
+    {
+        name: "Greensboro, Ga",
+        latitude: 33.57680717849283, 
+        longitude: -83.1835949886722
+    },
+    {
+        name: "Salem, NC",
+        latitude: 36.538062980835065, 
+        longitude: -80.60133781534029,
+    },
+    {
+        name: "Columbus, OH",
+        latitude: 39.98444035546748, 
+        longitude: -82.99154923423944,
+    },
+    {
+        name: "Englewood, NY",
+        latitude: 40.893266672535574, 
+        longitude: --73.96666051099037
+    },
+];
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const results = document.getElementById("results");
+    const cityDropdown = document.getElementById("cityDropdown");
+
+    
+});
